36278080f5f12a1df01795a627ce218bedf20803
root <root@localhost.localdomain>
2020-03-06 22:56:55 +0100

    Drawer and minor fixes

--- lib/home.dart
+++ lib/home.dart
@@ -172,7 +172,172 @@ class Home extends StatelessWidget {
           ],
         ),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: kHeight(context)*0.28,
+              padding: EdgeInsets.zero,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: kWidth(context),
+                    height: kHeight(context)*0.28,
+                    child: Image.asset(
+                      "assets/images/gurara.jpg",
+                      fit: BoxFit.cover,
+                      colorBlendMode: BlendMode.darken,
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: kHeight(context)*0.1,
+                      left: 5
+                    ),
+                    child: Center(
+                      child: Text(
+                          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed"
+                          " diam nonumy eirmod tempor invidunt ut labore et dolore.",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+            ),
+
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                size: 24,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.history,
+                size: 24,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                "History",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.tram,
+                size: 24,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                "Tourism",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                height: 20,
+                child: Image.asset(
+                  "assets/icons/groupX3.png",
+                  color: kPrimaryColor,
+                ),
+              ),
+              title: Text(
+                "People",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                height: 20,
+                child: Image.asset(
+                  "assets/icons/cultureX3.png",
+                  color: kPrimaryColor,
+                ),
+              ),
+              title: Text(
+                "Culture",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.map,
+                size: 24,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                "Map",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                height: 20,
+                child: Image.asset(
+                  "assets/icons/emergencyX3.png",
+                  color: kPrimaryColor,
+                ),
+              ),
+              title: Text(
+                "Emergency",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.camera_alt,
+                size: 24,
+                color: kPrimaryColor,
+              ),
+              title: Text(
+                "Gallery",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(100),
         child: CustomAppBar(),
